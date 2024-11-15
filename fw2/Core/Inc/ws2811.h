92c1b903a5f1de04c775a160f133b4cb663c0b5a
123p10 <obrake3@gmail.com>
2024-11-14 19:06:32 -0800

    Drip

---
+++ fw2/Core/Inc/ws2811.h
@@ -0,0 +1,22 @@
+#ifndef WS2811_H
+#define WS2811_H
+
+#include "stdint.h"
+
+#define NUMBER_LEDS (1)
+#define WS2811_BUFF_LEN (1)
+
+typedef struct {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+} led_code_S;
+
+typedef enum {
+	WS2811_RET_OK = 0,
+	WS2811_RET_FAILED = 1
+} ws2811_ret_E;
+
+ws2811_ret_E ws2811_tx(led_code_S (*led_timeseries)[NUMBER_LEDS], uint32_t n);
+ws2811_ret_E ws2811_trigger(void);
+#endif
---
+++ fw2/Core/Src/ws2811.c
@@ -0,0 +1,66 @@
+#include "ws2811.h"
+#include "main.h"
+#include "string.h"
+#include "stm32f0xx_hal_spi.h"
+
+#define BITS (4)
+#define BYTES_PER_LED (3)
+#define BUFF_SIZE (NUMBER_LEDS*BYTES_PER_LED*WS2811_BUFF_LEN)
+extern SPI_HandleTypeDef hspi1;
+
+static uint8_t ws2811_bkup_buffer[BUFF_SIZE];
+static uint8_t ws2811_buffer[BUFF_SIZE];
+
+static volatile uint32_t spi_index = 0;
+
+ws2811_ret_E ws2811_tx(led_code_S (*led_timeseries)[NUMBER_LEDS], uint32_t n) {
+	for (uint32_t i = 0; i < n; i++) {
+		for (uint32_t j = 0; j < NUMBER_LEDS; j++) {
+			ws2811_bkup_buffer[i * NUMBER_LEDS*BYTES_PER_LED + j*BYTES_PER_LED + 0] = led_timeseries[i][j].red;
+			ws2811_bkup_buffer[i * NUMBER_LEDS*BYTES_PER_LED + j*BYTES_PER_LED + 1] = led_timeseries[i][j].green;
+			ws2811_bkup_buffer[i * NUMBER_LEDS*BYTES_PER_LED + j*BYTES_PER_LED + 2] = led_timeseries[i][j].blue;
+		}
+	}
+	return WS2811_RET_OK;
+}
+
+static volatile uint8_t spi_d[8*BYTES_PER_LED*NUMBER_LEDS*BITS];
+static volatile uint8_t output_buff[BYTES_PER_LED*NUMBER_LEDS*BITS+3];
+ws2811_ret_E ws2811_trigger(void) {
+	uint8_t* tmp = &ws2811_bkup_buffer[spi_index*BYTES_PER_LED*NUMBER_LEDS];
+	for(uint32_t i = 0;i < BYTES_PER_LED*NUMBER_LEDS;i++) {
+		uint8_t mask = 0x80;
+		output_buff[i] = 0;
+		for(uint8_t j = 0; j < 8; j++) {
+			uint8_t bit = mask & tmp[i];
+			if (bit == 0) {
+				spi_d[i*8*BITS + j*BITS + 0] = 0;
+				spi_d[i*8*BITS + j*BITS + 1] = 0;
+				spi_d[i*8*BITS + j*BITS + 2] = 0;
+				spi_d[i*8*BITS + j*BITS + 3] = 1;
+			} else {
+				spi_d[i*8*BITS + j*BITS + 0] = 0;
+				spi_d[i*8*BITS + j*BITS + 1] = 0;
+				spi_d[i*8*BITS + j*BITS + 2] = 1;
+				spi_d[i*8*BITS + j*BITS + 3] = 1;
+			}
+			mask = mask >> 1;
+		}
+	}
+	uint32_t byte_i = 0;
+	uint8_t bit_i = 7;
+	for(uint32_t i = 0;i < 8*BYTES_PER_LED*NUMBER_LEDS*BITS;i++) {
+		output_buff[byte_i] = output_buff[byte_i] | (spi_d[i] << bit_i);
+		if (bit_i == 0) {
+			bit_i = 8;
+			byte_i++;
+		}
+		bit_i--;
+	}
+	uint32_t last_i = sizeof(output_buff)-1;
+	output_buff[last_i-2] = 0x00;
+	output_buff[last_i-1] = 0x00;
+	output_buff[last_i] = 0x00;
+	HAL_SPI_Transmit_IT(&hspi1, output_buff, BYTES_PER_LED*NUMBER_LEDS*BITS+3);
+	return WS2811_RET_OK;	
+}
